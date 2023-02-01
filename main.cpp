fe5c9c30ad04d7d12aef43387d5d926d80a36809
Chris <cbzoozoom7@gmail.com>
2023-01-31 21:54:21 -0600

    added main() stub.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// main.cpp
+// Chris Bowman
+// Created 31 Jan 2023
+// A box is not a rectangle.
+#include "main.h"
+int main(int argc, char** argv) {
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+// main.h
+// Chris Bowman
+// Created 31 Jan 2023
+// A box is not a rectangle.
+#ifndef CLASSES_MAIN
+    #define CLASSES_MAIN
+#endif //CLASSES_MAIN
