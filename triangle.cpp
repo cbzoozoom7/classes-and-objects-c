197d35acc38ca7cc9b60361f723a753f158f7e8a
Chris <cbzoozoom7@gmail.com>
2023-02-01 23:12:51 -0600

    guarded that each lengths must be greater than 0.

--- triangle.cpp
+++ triangle.cpp
@@ -14,14 +14,18 @@ Triangle::Triangle(double *lengths) {
 double Triangle::setSide(int s, double l) { //sets a side length, given the index of the side & the desired length
     if (s >= 0 || s <= 2) { //fail if index is invalid
         double sumOther;
-        for (int i = 0; i < 3; i++) { //find the sum of the lengths of the other 2 sides
-            sumOther += sides[i];
-        }
-        sumOther -= sides[s];
-        if (l > sumOther) { //If the desired length is longer than the sum, a triangle cannot be formed.
-            sides[s] = sumOther;
+        if (l <= 0) {
+            sides[s] = DEFAULT;
         } else {
-            sides[s] = l;
+            for (int i = 0; i < 3; i++) { //find the sum of the lengths of the other 2 sides
+                sumOther += sides[i];
+            }
+            sumOther -= sides[s];
+            if (l > sumOther) { //If the desired length is longer than the sum, a triangle cannot be formed.
+                sides[s] = sumOther;
+            } else {
+                sides[s] = l;
+            }
         }
     }
     return sides[s];
