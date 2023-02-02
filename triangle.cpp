a85a99ff4ec336d5454b8cecb2f6880d8cbfc325
Chris <cbzoozoom7@gmail.com>
2023-02-02 10:08:20 -0600

    moved away from the literal 3 & wrote the stub for Triangle::printTriangle()

--- triangle.cpp
+++ triangle.cpp
@@ -4,28 +4,28 @@
 // A box is not a rectangle.
 #include "triangle.h"
 Triangle::Triangle(double *lengths) {
-    for (int i = 0; i < 3; i++) { //set side lengths to the input because the setter requires the other values to be set to change one.
+    for (int i = 0; i < NUM_SIDES; i++) { //set side lengths to the input because the setter requires the other values to be set to change one.
         if (lengths[i] <= 0) { //The setter will not work correctly if any of the values are not positive.
             sides[i] = DEFAULT;
         } else {
             sides[i] = lengths[i];
         }
     }
-    for (int i = 0; i < 3; i++) { //run the setter to validate
+    for (int i = 0; i < NUM_SIDES; i++) { //run the setter to validate
         setSide(i, sides[i]);
     }
 };
 double Triangle::setSide(int s, double l) { //sets a side length, given the index of the side & the desired length
-    if (s >= 0 || s <= 2) { //fail if index is invalid
+    if (s >= 0 || s <= NUM_SIDES - 1) { //fail if index is invalid
         double sumOther;
         if (l <= 0) {
             sides[s] = DEFAULT;
         } else {
-            for (int i = 0; i < 3; i++) { //find the sum of the lengths of the other 2 sides
+            for (int i = 0; i < NUM_SIDES; i++) { //find the sum of the lengths of the other 2 sides
                 sumOther += sides[i];
             }
             sumOther -= sides[s];
-            if (l > sumOther) { //If the desired length is longer than the sum, a triangle cannot be formed.
+            if (l > sumOther || l <= 0) { //If the desired length is longer than the sum, a triangle cannot be formed.
                 sides[s] = sumOther;
             } else {
                 sides[s] = l;
@@ -33,4 +33,7 @@ double Triangle::setSide(int s, double l) { //sets a side length, given the inde
         }
     }
     return sides[s];
+}
+void Triangle::printTriangle() {
+
 }
--- triangle.h
+++ triangle.h
@@ -4,13 +4,23 @@
 // A box is not a rectangle.
 #ifndef CLASSES_TRIANGLE
     #define CLASSES_TRIANGLE
+    
     #define DEFAULT 1
+    #define NUM_SIDES 3
+
+    #include <iostream>
+
+    using std::cout;
+    using std::endl;
+
     class Triangle {
         public:
             //Constructors
             Triangle(double[3]);
             //Setters
             double setSide(int, double); //sets a side length, given the index of the side & the desired length
+            //Methods
+            void printTriangle();
         protected:
             double sides[3]; //the side lengths of the triangle
     };
